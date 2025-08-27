b913790204b3865af50788f2c4e497b233200a75
Chris Kelly <christopherkelly92@gmail.com>
2025-08-27 17:52:19 -0400

    Initial Commit

---
+++ Vinyl Wall/Vinyl_WallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vinyl_WallApp.swift
+//  Vinyl Wall
+//
+//  Created by Chris Kelly on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Vinyl_WallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
