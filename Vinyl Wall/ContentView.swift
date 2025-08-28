75fd5b53075b448978344eb4f727d2ac193895ab
chriskelly <christopherkelly92@gmail.com>
2025-08-28 09:51:14 -0400

    feat: Add placeholder album management functionality with a list view in ContentView. Implemented AlbumTile for displaying album details and integrated UserDefaults for persistent storage of albums.

--- Vinyl Wall/ContentView.swift
+++ Vinyl Wall/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var albums = AlbumsPlaceholder()
+
+        var body: some View {
+            NavigationStack {
+                List {
+                    ForEach(albums.items) {
+                        AlbumTile(album: $0)
+                    }
+                    .onDelete { indexSet in
+                        albums.items.remove(atOffsets: indexSet)
+                    }
+                }
+                .navigationTitle("Fav Albums")
+                .toolbar {
+                    Button("Add album", systemImage: "plus") {
+                        albums.items.append(AlbumPlaceholder(title: "New Album", artistName: "Unknown"))
+                    }
+                }
+            }
         }
-        .padding()
+    }
+
+struct AlbumTile: View {
+    let album: AlbumPlaceholder
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(album.title)
+                    .font(.headline)
+                Text(album.artistName)
+                    .font(.footnote)
+            }
+            Spacer()
+            Text(album.artworkURL)
+        }
+    }
+}
+
+struct AlbumPlaceholder: Identifiable, Codable {
+    var id = UUID()
+    let title: String
+    let artistName: String
+    var artworkURL = "O"
+}
+
+@Observable
+class AlbumsPlaceholder {
+    static let itemsKey = "itemsKey"
+    
+    var items = [AlbumPlaceholder]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: AlbumsPlaceholder.itemsKey)
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: AlbumsPlaceholder.itemsKey) {
+            if let decoded = try? JSONDecoder().decode([AlbumPlaceholder].self, from: savedItems) {
+                items = decoded
+                return
+            }
+        }
+        items = [
+            AlbumPlaceholder(title: "Take Care", artistName: "Drake"),
+            AlbumPlaceholder(title: "Born Sinner", artistName: "J. Cole")
+        ]
     }
 }
 
